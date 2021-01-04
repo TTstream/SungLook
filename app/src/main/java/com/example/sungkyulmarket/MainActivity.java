8e882dafae48144f287527872b419807df5d9116
sk0932 <sk0932@sungkyul.ac.kr>
2021-01-04 15:35:29 +0900

    productlookj 생성

---
+++ app/src/main/java/com/example/sungkyulmarket/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sungkyulmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.productlook);
+    }
+}
